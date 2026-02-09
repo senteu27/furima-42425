d50caba5bada30e75848f9c1fdd5921585ad103d
senteu27 <hosomiakashi1227@icloud.com>
2026-02-09 10:20:15 +0900

    バリデーションの追加と修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ has_many  :buys
 
 validates :nickname, presence: true
 validates :password, format:{with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/,message:"は数字と英語を含むようにしてください。"}
-validates :first_name, presence: true
-validates :last_name, presence: true
-validates :first_name_kana, presence: true
-validates :last_name_kana, presence: true
+validates :first_name, presence: true, format:{with:/\A[ぁ-んァ-ヶー一-龥]+\z/,message:"はひらがな、カタカナ、漢字のみで記入してください。"}
+validates :last_name, presence: true, format:{with:/\A[ぁ-んァ-ヶー一-龥]+\z/,message:"はひらがな、カタカナ、漢字のみで記入してください。"}
+validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/, message:"はカタカナで記入してください。"}
+validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー]+\z/, message:"はカタカナで記入してください。"}
 validates :birthday, presence: true
 end

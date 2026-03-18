3064686e95780dd41cca0ad3c0474fd83e0caf92
senteu27 <hosomiakashi1227@icloud.com>
2026-03-18 10:20:47 +0900

    商品画像を付けないと商品登録できないように修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ApplicationRecord
   validates :shipping_day_id, numericality: {other_than: 1, message: "can t be blank"}
   validates :prefecture_id, numericality: {other_than: 1, message: "can t be blank"}
   validates :shipping_prise_id, numericality: {other_than: 1, message: "can t be blank"}
+  validates :image
   end
 
   belongs_to :user

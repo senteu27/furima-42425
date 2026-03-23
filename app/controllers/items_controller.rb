5cc6f1b495ffa50c96f0a93676cf9eb89d8a93ca
senteu27 <hosomiakashi1227@icloud.com>
2026-03-23 11:28:21 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,11 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   def index
   end
+
   def new
     @item = Item.new
   end
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -13,9 +15,11 @@ class ItemsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :price, :condition_description_id, :category_id, :shipping_day_id, :prefecture_id, :shipping_prise_id, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :price, :condition_description_id, :category_id, :shipping_day_id,
+                                 :prefecture_id, :shipping_prise_id, :image).merge(user_id: current_user.id)
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,66 +5,66 @@ RSpec.describe Item, type: :model do
     @item = FactoryBot.build(:item)
   end
 
-  describe "商品出品機能" do
-    context "商品出品ができない時" do
-      it "商品画像が空だと出品できない" do
-        @item.image = ""
+  describe '商品出品機能' do
+    context '商品出品ができない時' do
+      it '商品画像が空だと出品できない' do
+        @item.image = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it "商品の名前が空だと出品できない" do
-        @item.name = ""
+      it '商品の名前が空だと出品できない' do
+        @item.name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Name can't be blank")
       end
-      it "商品の説明が空だと出品できない" do
-        @item.description = ""
+      it '商品の説明が空だと出品できない' do
+        @item.description = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
-      it "カテゴリーが1だと出品できない" do
+      it 'カテゴリーが1だと出品できない' do
         @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it "商品の状態が1だと出品できない" do
+      it '商品の状態が1だと出品できない' do
         @item.condition_description_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition description can't be blank")
       end
-      it "配送料が1だと出品できない" do
+      it '配送料が1だと出品できない' do
         @item.shipping_prise_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping prise can't be blank")
       end
-      it "発送元が1だと出品できない" do
+      it '発送元が1だと出品できない' do
         @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
-      it "発送までの日数が1だと出品できない" do
+      it '発送までの日数が1だと出品できない' do
         @item.shipping_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping day can't be blank")
       end
-      it "商品の価格が300未満だと出品できない" do
+      it '商品の価格が300未満だと出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is invalid")
+        expect(@item.errors.full_messages).to include('Price is invalid')
       end
-      it "商品の価格が9999999" do
-        @item.price = 10000000
+      it '商品の価格が9999999' do
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is invalid")
+        expect(@item.errors.full_messages).to include('Price is invalid')
       end
-      it "価格は半角数字じゃないと出品できない" do
-        @item.price = "１２３４５"
+      it '価格は半角数字じゃないと出品できない' do
+        @item.price = '１２３４５'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is invalid")
+        expect(@item.errors.full_messages).to include('Price is invalid')
       end
     end
-    context "商品出品出来る時" do
-      it "必要な情報を入力すれば出品できる" do
+    context '商品出品出来る時' do
+      it '必要な情報を入力すれば出品できる' do
         expect(@item).to be_valid
       end
     end

2a27fe73c00f2936c012a2df8f15fcf587d7522d
senteu27 <hosomiakashi1227@icloud.com>
2026-04-08 13:41:10 +0900

    rubocopの実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -65,7 +65,7 @@ RSpec.describe Item, type: :model do
       it 'userが紐づいていないと出品できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
     context '商品出品出来る時' do

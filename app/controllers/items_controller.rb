a51669d3428f0635c16fc3855d662857195fa9d4
senteu27 <hosomiakashi1227@icloud.com>
2026-04-13 10:51:35 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,10 +15,11 @@ class ItemsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
   def show
     @item =Item.find(params[:id])
   end
-end
 
   private
 

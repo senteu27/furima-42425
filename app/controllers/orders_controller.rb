928197a6393a1afa5b960e6b664cabb0fc5e162a
senteu27 <hosomiakashi1227@icloud.com>
2026-05-25 11:34:17 +0900

    商品の配送先、購入したユーザー、購入された商品情報をdbに保存できるよう設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,5 +2,23 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_destination = OrderDestination.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_destination = OrderDestination.new(order_params)
+    if @order_destination.valid?
+      @order_destination.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_destination).permit(:postal_code, :prefecture_id, :municipalities,:street_address, :building_name,:telephone_number).merge(user_id: current_user.id, item_id: (@item.id) )
   end
 end
---
+++ app/models/order_destination.rb
@@ -0,0 +1,21 @@
+class OrderDestination
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number, :item_id, :user_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 0, message: "can' t be blank"}
+    validates :municipalities
+    validates :street_address
+    validates :telephone_number, format: {with: /\A[0-9]{10,11}\z/,message: "is invalid"}
+  end
+
+    def save
+
+    order =Order.create(user_id: user_id, item_id: item_id)
+
+    Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, 
+    street_address: street_address, building_name: building_name, 
+    telephone_number: telephone_number, order_id: order.id)
+  end
+end

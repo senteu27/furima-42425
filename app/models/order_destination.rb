d9647682f52601998b46a092626b6dbe54dd712e
senteu27 <hosomiakashi1227@icloud.com>
2026-06-03 14:37:27 +0900

    商品購入機能のテストコード

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -4,12 +4,13 @@ class OrderDestination
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id, numericality: {other_than: 1, message: "can' t be blank"}
     validates :municipalities
     validates :street_address
     validates :telephone_number, format: {with: /\A[0-9]{10,11}\z/,message: "is invalid"}
     validates :token
   end
+
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
     def save
 
---
+++ spec/models/order_destination_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe OrderDestination, type: :model do
+  before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @order_destination = FactoryBot.build(:order_destination, user_id: user.id, item_id: item.id)
+  end
+
+  describe '商品購入機能' do
+
+    context '商品購入できない時' do
+      it '郵便番号が空では購入できない' do
+        @order_destination.postal_code = ''
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it '郵便番号にハイフンが含まれていないと購入できない' do
+        @order_destination.postal_code = '1234567'
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+      end
+      it '郵便番号が全角では購入できない' do
+        @order_destination.postal_code = '１２３４-５６７'
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+      end
+      it '都道府県が1では購入できない' do
+        @order_destination.prefecture_id = 1
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '市町村区が空では購入できない' do
+        @order_destination.municipalities = ''
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Municipalities can't be blank")
+      end
+      it '番地が空では購入できない' do
+        @order_destination.street_address = ''
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Street address can't be blank")
+      end
+      it '電話番号が空では購入できない' do
+        @order_destination.telephone_number = ''
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Telephone number can't be blank")
+      end
+      it '電話番号は半角数字のみじゃないと購入できない' do
+        @order_destination.telephone_number = '090-1234-5678'
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Telephone number is invalid")
+      end
+      it '電話番号が全角では購入できない' do
+        @order_destination.telephone_number = '０９０１２３４５６７８'
+        @order_destination.valid?
+        expect(@order_destination.errors.full_messages).to include("Telephone number is invalid")
+      end
+    end
+    context '商品購入できるとき' do
+      it '必要な情報を入力すれば購入できる' do
+        expect(@order_destination).to be_valid
+      end
+      it '建物名はなくても購入できる' do
+        @order_destination.building_name = ''
+        expect(@order_destination).to be_valid
+      end
+    end
+  end
+end

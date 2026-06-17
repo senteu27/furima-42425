d645d81d35978c04533c364bd4059f120a526778
senteu27 <hosomiakashi1227@icloud.com>
2026-06-17 13:39:05 +0900

    確認用のコードを元に戻す

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -4,40 +4,15 @@ const pay = () => {
   if (!form) {
     return;
   }
-  const publickey = gon.public_key
-  console.log("1");
-
-const payjp = Payjp(publickey);
-
-console.log("2");
-
-const elements = payjp.elements();
-
-console.log("3");
-
-const numberElement = elements.create('cardNumber');
-
-console.log("4");
-
-const expiryElement = elements.create('cardExpiry');
-
-console.log("5");
-
-const cvcElement = elements.create('cardCvc');
-
-console.log("6");
-
-numberElement.mount('#number-form');
-
-console.log("7");
-
-expiryElement.mount('#expiry-form');
-
-console.log("8");
-
-cvcElement.mount('#cvc-form');
-
-console.log("9");
+  const publickey = gon.public_key 
+  const payjp = Payjp(publickey); 
+  const elements = payjp.elements(); 
+  const numberElement = elements.create('cardNumber'); 
+  const expiryElement = elements.create('cardExpiry'); 
+  const cvcElement = elements.create('cardCvc'); 
+  numberElement.mount('#number-form'); 
+  expiryElement.mount('#expiry-form'); 
+  cvcElement.mount('#cvc-form');
 
   form.addEventListener("submit", (e) => {
     payjp.createToken(numberElement).then(function (response) {

56526b3cac40fd3f0ccd4199e4dea2d6065c5bc5
senteu27 <hosomiakashi1227@icloud.com>
2026-06-17 11:44:49 +0900

    確認用

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -5,15 +5,39 @@ const pay = () => {
     return;
   }
   const publickey = gon.public_key
-  const payjp = Payjp(publickey);
-  const elements = payjp.elements();
-  const numberElement = elements.create('cardNumber');
-  const expiryElement = elements.create('cardExpiry');
-  const cvcElement = elements.create('cardCvc');
+  console.log("1");
 
-  numberElement.mount('#number-form');
-  expiryElement.mount('#expiry-form');
-  cvcElement.mount('#cvc-form');
+const payjp = Payjp(publickey);
+
+console.log("2");
+
+const elements = payjp.elements();
+
+console.log("3");
+
+const numberElement = elements.create('cardNumber');
+
+console.log("4");
+
+const expiryElement = elements.create('cardExpiry');
+
+console.log("5");
+
+const cvcElement = elements.create('cardCvc');
+
+console.log("6");
+
+numberElement.mount('#number-form');
+
+console.log("7");
+
+expiryElement.mount('#expiry-form');
+
+console.log("8");
+
+cvcElement.mount('#cvc-form');
+
+console.log("9");
 
   form.addEventListener("submit", (e) => {
     payjp.createToken(numberElement).then(function (response) {
